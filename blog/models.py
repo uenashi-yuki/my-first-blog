c250c36232cd76b4be17b273d3ee20ed6c4444f9
uenashi.yuki@gmail.com <uenashi.yuki@gmail.com>
2022-02-28 20:13:38 +0900

    My Django Girls app, first commit

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from operator import truediv
+from re import T
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    chosya = models.CharField(max_length=50, null=True)
+    furigana = models.CharField(max_length=50, null=True)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
